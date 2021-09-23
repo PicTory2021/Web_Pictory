edfb51be69c92425bd7f615e5cabc16a828922c1
yeonui-0626 <lye0626@gmail.com>
2021-09-23 14:19:19 +0900

    임시저장

---
+++ djangoRS/db.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pictory',
+        'USER': 'yeon',
+        'PASSWORD': 'now0913', # 비번 입력
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
