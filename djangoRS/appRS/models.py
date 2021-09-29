ae185cf6e7ec60fbe973a7db146e9e274d383353
yeonui-0626 <73818604+yeonui-0626@users.noreply.github.com>
2021-09-30 00:18:19 +0900

    Update models.py

--- djangoRS/appRS/models.py
+++ djangoRS/appRS/models.py
@@ -13,5 +13,3 @@ class Image(models.Model):
     Contents = models.TextField(null=True)
     Latitude = models.DecimalField(max_digits=30, decimal_places=18, null=True); #위도
     Longitude = models.DecimalField(max_digits=30, decimal_places=18, null=True); #경도
-    Latitude = models.DecimalField(max_digits=30, decimal_places=18, null=True); #위도
-    Longitude = models.DecimalField(max_digits=30, decimal_places=18, null=True); #경도
