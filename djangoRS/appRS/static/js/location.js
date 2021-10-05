bef83b597438b1cef76ca22cd8a96874c15617f4
yeonui-0626 <lye0626@gmail.com>
2021-10-06 04:40:14 +0900

    index-주변 관광지 제목 출력

--- djangoRS/appRS/static/js/location.js
+++ djangoRS/appRS/static/js/location.js
@@ -3,7 +3,10 @@ var gps_lat = null; // 위도
 var gps_lng = null; // 경도
 var gps_position; // gps 위치 객체
 
-gps_check();
+$("document").ready(function(){
+    gps_check();
+})
+
 // gps가 이용가능한지 체크하는 함수이며, 이용가능하다면 show location 함수를 불러온다.
 // 만약 작동되지 않는다면 경고창을 띄우고, 에러가 있다면 errorHandler 함수를 불러온다.
 // timeout을 통해 시간제한을 둔다.
@@ -48,6 +51,8 @@ function showLocation(position) {
                     "                    </section>"
             }
             $('[class="row"]').html(showImage);
+            const header = document.querySelector(".nearLocHeader")
+            header.innerText="주변 관광지"
         },
         error: function (request, status, err) {
             console.log(err);
@@ -59,11 +64,11 @@ function showLocation(position) {
 
 
 // error발생 시 에러의 종류를 알려주는 함수.
-// function errorHandler(error) {
-//     if(error.code == 1) {
-//         alert("접근차단");
-//     } else if( err.code == 2) {
-//         alert("위치를 반환할 수 없습니다.");
-//     }
-//     gps_use = false;
-// }
+function errorHandler(error) {
+    // if(error.code == 1) {
+    //     alert("접근차단");
+    // } else if( err.code == 2) {
+    //     alert("위치를 반환할 수 없습니다.");
+    // }
+    // gps_use = false;
+}
